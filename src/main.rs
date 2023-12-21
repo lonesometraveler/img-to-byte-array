824dd3b89532de0983293bc977c40edb720a9804
Kentaro Okuda <lonesometraveler@mac.com>
2023-12-21 17:57:46 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
-use std::env;
-use std::process;
+mod img2bytes;
 
-use img2bytes::ImgToBytes;
+use std::env;
 
 fn main() {
-    let writer = ImgToBytes::new(env::args()).unwrap_or_else(|err| {
-        eprintln!("problem parsing arguments: {}", err);
-        process::exit(1);
-    });
+    let args: Vec<String> = env::args().collect();
 
-    let output = writer.run();
-    println!("{}", output);
+    let src_file_path = args
+        .get(1)
+        .expect("Usage: cargo run path_to_image_folder name_of_array > filename_to_be_saved.h");
+    let array_name = args
+        .get(2)
+        .expect("Usage: cargo run path_to_image_folder name_of_array > filename_to_be_saved.h");
+
+    match img2bytes::convert(src_file_path, array_name) {
+        // Write to file
+        Ok(result) => println!("{}", result),
+        Err(err) => eprintln!("Error: {}", err),
+    }
 }
